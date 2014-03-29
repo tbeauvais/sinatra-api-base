9dd8ab1c1b4bda099a2825149b23f7092553bb28
tbeauvais <tbeauvais@yahoo.com>
2014-03-29 11:24:13 -0400

    Added call to redis service

--- hello_app.rb
+++ hello_app.rb
@@ -1,6 +1,23 @@
 #
 require 'sinatra'
+require 'json'
+require 'redis'
+
+
+configure do
+
+    credentials = {"credentials"=>{"port"=>"6379", "hostname"=>"localhost", "password"=>""}}
+
+    if ENV['VCAP_SERVICES']
+      rediscloud_service = JSON.parse(ENV['VCAP_SERVICES'])["rediscloud-n/a"]
+      credentials = rediscloud_service.first["credentials"]
+    end
+
+    puts credentials.to_s
+    $redis = Redis.new(:host => credentials["hostname"], :port => credentials["port"], :password => credentials["password"])
+end
 
 get '/' do
-  'Hello world!'
+   $redis.set("foo", "bar")
+  'Hello world! ' + $redis.get('foo')
 end
